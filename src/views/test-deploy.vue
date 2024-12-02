31774fcd9b66fba0273bac0ae9ebbf5db809b470
kawamotor-bit <kawamotor@yamaha-motor.co.jp>
2024-12-02 13:46:51 +0900

    [AL00587(AL00587) on 20241202044650]test

---
+++ src/views/test-deploy.vue
@@ -0,0 +1,24 @@
+<script setup>
+import { useLockScreen } from 'viy-ui';
+import { useI18n } from 'vue-i18n';
+
+const { t } = useI18n();
+const { lockScreen } = useLockScreen();
+
+defineOptions({
+  name: 'test-deploy',
+});
+
+const form = ref();
+
+const formData = reactive({
+});
+</script>
+
+<template>
+  <VueForm ref="form" v-loading="lockScreen" :model="formData">
+    <VueText id="viy2Text_7LAyL" ref="viy2Text_7LAyL" :style="{ width: '100%', display: 'inline-block' }">
+      Text
+    </VueText>
+  </VueForm>
+</template>
